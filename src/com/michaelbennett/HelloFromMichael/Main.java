ecbeec5bdf1506585e47b9c80664caa57e0d3776
Michael-Bennett-BYU-I <91813824+Michael-Bennett-BYU-I@users.noreply.github.com>
2021-10-07 22:15:06 -0700

    just adding stuff.

--- src/com/michaelbennett/HelloFromMichael/Main.java
+++ src/com/michaelbennett/HelloFromMichael/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         printMyName();
+
+        printYourName("this is an example.");
     }
 
     public static void printMyName() {
