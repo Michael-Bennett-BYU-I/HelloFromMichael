d14a1579be12a913cd2205f859dfd5d900abab21
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-10-09 13:49:12 -0500

    Hello from Nathan!

--- src/com/michaelbennett/HelloFromMichael/Main.java
+++ src/com/michaelbennett/HelloFromMichael/Main.java
@@ -7,6 +7,9 @@ public class Main {
         printMyName();
 
         printYourName("this is an example.");
+
+        // from Nathan:
+        printYourName("Nathan!");
     }
 
     public static void printMyName() {
